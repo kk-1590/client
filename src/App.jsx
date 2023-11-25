cc504ef237c7df8711342231893748f9db4e8be7
kk-1590 <kkhandelwal1590@gmail.com>
2023-11-25 23:59:48 +0530

    initialised the project and completed setup

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom"
+
+import {
+  HomeLayout,Landing,Register,Login,DashboardLayout,Error
+} from './pages'
+
+const router = createBrowserRouter([
+  {
+    path : '/',
+    element : <HomeLayout/>,
+  },  
+  {
+    path : '/register',
+    element : <Register/>,
+  }, 
+  {
+    path : '/login',
+    element : <Login/>,
+  }, 
+  {
+    path : '/dashboard',
+    element : <DashboardLayout/>,
+  },
+])
+
+const App = () => {
+  return <RouterProvider router={router}></RouterProvider>
+}
+
+export default App
