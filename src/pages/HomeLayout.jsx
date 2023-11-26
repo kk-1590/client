8cf974e27b96b647ed929aff077452c5b5512079
kk-1590 <kkhandelwal1590@gmail.com>
2023-11-27 00:11:47 +0530

    Landing page completed

--- src/pages/HomeLayout.jsx
+++ src/pages/HomeLayout.jsx
@@ -2,10 +2,9 @@ import { Outlet } from "react-router-dom"
 
 const HomeLayout = () => {
   return (
-    <div>
-      <navbar>navbar</navbar>
+    <>
       <Outlet />
-    </div>
+    </>
   )
 }
 
--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,9 +1,38 @@
-import React from 'react'
+import styled from "styled-components";
+import Wrapper from "../assets/wrappers/LandingPage";
+import main from "../assets/images/main.svg";
+import logo from "../assets/images/logo.svg";
+import { Link } from "react-router-dom";
 
 const Landing = () => {
   return (
-    <h1>Landing Page</h1>
-  )
-}
+    <Wrapper>
+      <nav>
+        <img src={logo} alt="jobstack" className="logo" />
+      </nav>
+      <div className="container page">
+        <div className="info">
+          <h1>
+            job <span>tracking</span> app
+          </h1>
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repellat,
+            consectetur pariatur perferendis tenetur dicta repudiandae omnis
+            nihil odio quaerat, ab vel laborum esse iusto beatae cupiditate
+            ipsum eos recusandae necessitatibus! Lorem ipsum dolor sit amet
+            consectetur, adipisicing elit. Aspernatur, cum.
+          </p>
+          <Link to="/register" className="btn register-link">
+            Register
+          </Link>
+          <Link to="/login" className="btn">
+            Login / Demo User
+          </Link>
+        </div>
+        <img src={main} alt="job hunt" className="img main-img" />
+      </div>
+    </Wrapper>
+  );
+};
 
-export default Landing
+export default Landing;
